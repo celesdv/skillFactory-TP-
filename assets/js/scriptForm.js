27d7b081c6d4eae08ed6bdb27e611e419d2f5199
Celeste Dangelo <celesdv@gmail.com>
2022-08-10 20:54:11 -0300

    :sparkles: add scrip form to capture form info

---
+++ assets/js/scriptForm.js
@@ -0,0 +1,8 @@
+function captura(){
+    var user = document.getElementById("user").value;
+    console.log("User: ", user);
+    var email = document.getElementById("email").value;
+    console.log("Email: ", email);
+    var comment = document.getElementById("comment").value;
+    console.log("Comment: ", comment);
+}
